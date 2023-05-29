e53937f380defaf79e3cb426a7556fe8cc2fa29f
Augusto Camargo <augusto.camargo2010@gmail.com>
2023-05-29 12:50:17 -0300

    Implementação IsCollected e enum

--- jewel.cs
+++ jewel.cs
@@ -3,21 +3,39 @@ namespace JewelCollector
     class Jewel : Item, IUsable //alteração feita por AugustoCamargo2
     {
         public int Points { get; }
+        public JewelType Type { get; }
+        public bool IsCollected { get; private set; }
 
         public Jewel(string symbol, int points) : base(symbol)
         {
             Points = points;
+            Type = type;
+            IsCollected = false;
         }
 
         public void Use(Robot robot)
         {
-            robot.CollectItem(this);
-            robot.Item(points); //alteração feita por AugustoCamargo1
+            if(!IsCollected)
+            {
+                robot.CollectItem(this);
+                IsCollected = true; //alteração feita por AugustoCamargo1
+            }
+            else
+            {
+                robot.RechargeEnergy(Points);
+            }
         }
 
         public override string ToString()
         {
-            return $"J{Points}"; //Alteração feita por AugustoCamargo2
+            string status = IsCollected ? "Coletado" : "Disponivel";
+            return $"Jewel: Type ={Type}, Points = {Points}, Status ={status}"; //Alteração feita por AugustoCamargo2
         }
+    } //Alteração feita por AugustoCamargo2
+    enum JewelType
+    {
+        Red,
+        Green,
+        Blue
     }
 }
